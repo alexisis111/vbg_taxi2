c046a4ca609c037613d773253293e5b4cc476dbb
alexisis111 <shuragilyow1@gmail.com>
2024-09-02 18:13:42 +0300

    Add netlify.toml for redirects

--- src/components/testComponent/LocationPicker.jsx
+++ src/components/testComponent/LocationPicker.jsx
@@ -2,7 +2,7 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, useMap, useMapEvents } from 'react-leaflet';
 import { useState, useEffect } from 'react';
-import '../../App.css'
+
 // Импорт иконок
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
@@ -77,33 +77,34 @@ const LocationPicker = () => {
                     value={pickup}
                     onChange={handlePickupChange}
                     placeholder="Откуда вас забрать?"
-                    className="w-full p-2 mb-2 border rounded focus:outline-none"
+                    className="w-full p-2 mb-2 border rounded text-black placeholder-gray-500 focus:outline-none"
                 />
                 <input
                     type="text"
                     value={dropoff}
                     onChange={handleDropoffChange}
                     placeholder="Куда отвезти?"
-                    className="w-full p-2 border rounded focus:outline-none"
+                    className="w-full p-2 border rounded text-black placeholder-gray-500 focus:outline-none"
                 />
             </div>
+
             <div className="relative w-full h-screen">
                 <MapContainer
                     center={[51.505, -0.09]} // Исходное положение
                     zoom={13}
                     className="w-full h-full"
-                    style={{ marginTop: '8rem' }} // Отступ карты вниз на высоту инпутов
+                    style={{marginTop: '8rem'}} // Отступ карты вниз на высоту инпутов
                 >
                     <TileLayer
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                         attribution="&copy; OpenStreetMap contributors"
                     />
-                    {userLocation && <Marker position={userLocation} />}
-                    {pickupCoords && <Marker position={pickupCoords} />}
-                    {dropoffCoords && <Marker position={dropoffCoords} />}
-                    <LocationMarker setCoords={setPickupCoords} />
-                    <LocationMarker setCoords={setDropoffCoords} />
-                    <MapUpdater location={userLocation} /> {/* Центрирование карты на местоположении */}
+                    {userLocation && <Marker position={userLocation}/>}
+                    {pickupCoords && <Marker position={pickupCoords}/>}
+                    {dropoffCoords && <Marker position={dropoffCoords}/>}
+                    <LocationMarker setCoords={setPickupCoords}/>
+                    <LocationMarker setCoords={setDropoffCoords}/>
+                    <MapUpdater location={userLocation}/> {/* Центрирование карты на местоположении */}
                 </MapContainer>
             </div>
         </>
