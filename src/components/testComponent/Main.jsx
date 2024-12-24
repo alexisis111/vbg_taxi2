e639da76c05f25e7831786e76fd3948c5c689dff
alexisis111 <shuragilyow1@gmail.com>
2024-12-24 13:30:04 +0300

    замена ngrok tunnel addr

--- src/components/testComponent/Main.jsx
+++ src/components/testComponent/Main.jsx
@@ -14,7 +14,7 @@ const Main = () => {
             queryId,
             userId
         }
-        fetch('https://34cb-185-108-19-43.ngrok-free.app/web-data', {
+        fetch('https://2627-185-108-19-43.ngrok-free.app/web-data', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
